67a0f2483812c4d27f05905d410f68c17f2bbf2f
huy <huythse180580@fpt.edu.vn>
2025-06-20 16:53:09 +0700

    Update axiosClient.tsx

--- src/servers/axiosClient.tsx
+++ src/servers/axiosClient.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:5188/api",
+  baseURL: "https://localhost:7065/api",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
